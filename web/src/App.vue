4fc65ea54d77d04f7b52184e4ffd39c5263bc522
wuyi651 <liuwuyi386@gmail.com>
2022-09-06 19:36:16 -0700

    skeleton prepared

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <div>Bot name :</div>
+    <div>Bot Battle Power :</div>
+  </div>
   <router-view/>
 </template>
 
+
+<script>
+export default {
+  name:"App",
+  setup:() => {
+
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("@/assets/background.png");
+  background-size: cover;
 }
 </style>
+
