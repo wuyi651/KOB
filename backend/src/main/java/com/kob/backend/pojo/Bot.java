fc6fc949a64c26f5de39210f018e175cca0a8989
wuyi651 <liuwuyi386@gmail.com>
2022-09-09 22:50:49 -0700

    complete my account page

--- backend/src/main/java/com/kob/backend/pojo/Bot.java
+++ backend/src/main/java/com/kob/backend/pojo/Bot.java
@@ -26,9 +26,9 @@ public class Bot {
     private String description;
     private String content;
     private Integer rating;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Seattle")
     private Date createtime;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Seattle")
     private Date modifytime;
 }
 
